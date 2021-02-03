40744bda8d56724ecfc616a50469f94300afcf82
Rachel Kozlowsky <rachelkozlowsky@gmail.com>
2021-02-02 23:02:55 -0300

    exercicio c#

---
+++ Girls.Gama2/Entidades/Dinheiro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girls.Gama2.Entidades
+{
+    public class Dinheiro
+    {
+        public Dinheiro(string cpf,
+                     double valor,
+                     string descricao,
+                     double valor_recebido)
+        {
+            Cpf = cpf;
+            Valor = valor;
+            Descricao = descricao;
+            ValorRecebido = valor_recebido;
+
+           
+        }
+        public string Cpf { get; set; }
+        public double Valor { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public string Descricao { get; set; }
+        public double ValorRecebido { get; set; }
+
+        public void Pagar()
+        {
+            DataPagamento = DateTime.Now;
+            if (ValorRecebido > Valor)
+            {
+                var troco = ValorRecebido - Valor;
+                Console.WriteLine($"Retornar troco: {troco}");
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ Girls.Gama2/Program.cs
@@ -0,0 +1,249 @@
+﻿using Girls.Gama2.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Girls.Gama2
+{
+    class Program
+    {
+        private static List<Boleto> listaBoletos;
+        private static List<Dinheiro> listaDinheiros;
+        static void Main(string[] args)
+        {
+            listaBoletos = new List<Boleto>();
+            listaDinheiros = new List<Dinheiro>();
+
+            while (true)
+            {
+                Console.WriteLine("================== Loja das meninas da Gama Academy ============================");
+                Console.WriteLine("Selecione uma opção");
+                Console.WriteLine("1-Compra | 2-Pagamento | 3-Relatório");
+
+                var opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Comprar();
+                        break;
+                    case 2:
+                        Pagamento();
+                        break;
+                    case 3:
+                        Relatorio();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void Comprar()
+        {
+            Console.WriteLine("Escolha a forma de pagamento:");
+
+            Console.WriteLine("1-Dinheiro | 2-Boleto");
+
+            var opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    PagamentoDinheiro();
+                    break;
+                case 2:
+                    PagamentoBoleto();
+                    break;
+                default:
+                    break;
+            }
+
+
+            
+        }
+
+        public static void PagamentoDinheiro()
+        {
+            Console.WriteLine("Digite o valor da compra:");
+            var valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor recebido pelo Cliente:");
+            var valor_recebido = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o CPF do cliente:");
+            var cpf = Console.ReadLine();
+
+            Console.WriteLine("Preeencha uma descrição caso necessário");
+            var descricao = Console.ReadLine();
+
+            var dinheiro = new Dinheiro(cpf, valor, descricao, valor_recebido);
+            dinheiro.Pagar();
+
+             Console.WriteLine($"Pagamento realizado {dinheiro.DataPagamento} com sucesso \n");
+
+            listaDinheiros.Add(dinheiro);
+        }
+
+
+        public static void PagamentoBoleto()
+        {
+            Console.WriteLine("Digite o valor da compra:");
+            var valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o CPF do cliente:");
+            var cpf = Console.ReadLine();
+
+            Console.WriteLine("Preeencha uma descrição caso necessário");
+            var descricao = Console.ReadLine();
+
+            var boleto = new Boleto(cpf, valor, descricao);
+            boleto.GerarBoleto();
+
+            Console.WriteLine($"Boleto gerado com sucesso com o número {boleto.CodigoBarra} com data de vencimento para o dia {boleto.DataVencimento} ");
+
+            listaBoletos.Add(boleto);
+        }
+
+        public static void Pagamento()
+        {
+            Console.WriteLine("Digite o código de barras:");
+            var numero = Guid.Parse(Console.ReadLine());
+
+            var boleto = listaBoletos
+                            .Where(item => item.CodigoBarra == numero)
+                            .FirstOrDefault();
+
+            if(boleto is null)
+            {
+                Console.WriteLine($"Boleto de código {numero} não encontrado!");
+                return;
+            }
+
+            if(boleto.EstaPago())
+            {
+                Console.WriteLine($"Boleto já foi pago no dia {boleto.DataPagamento}");
+                return;
+            }
+
+            if (boleto.EstaVencido())
+            {
+                boleto.CalcularJuros();
+                Console.WriteLine($"Boleto está vencido, terá acrescimo de 10% === R$ {boleto.Valor}");
+            }
+
+            boleto.Pagar();
+            Console.WriteLine($"Boleto de código {numero} foi pago com sucesso");
+        }
+
+        public static void Relatorio()
+        {
+            Console.WriteLine("Qual opção de relatório:");
+            Console.WriteLine("1-Pagamentos em Dinheiro | 2-Pagamentos em Boleto");
+
+            var opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    PagamentosDinheiro();
+                    break;
+                case 2:
+                    PagamentosBoletos();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+        public static void PagamentosDinheiro()
+        {
+            Console.WriteLine("========== Pagamentos em Dinheiro ============");
+            var dinheiros = listaDinheiros.ToList();
+
+            foreach (var item in dinheiros)
+            {
+                Console.WriteLine("\n ====");
+                Console.WriteLine($"CPF: {item.Cpf}\nValor:{item.Valor}\nData Pagamento: {item.DataPagamento} ==");
+            }
+
+            Console.WriteLine("========== Pagamentos em Dinheiro ============ \n");
+        }
+
+        public static void PagamentosBoletos()
+        {
+                Console.WriteLine("1-Pagos | 2-À pagar | 3-Vencidos");
+
+                var opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        BoletosPagos();
+                        break;
+                    case 2:
+                        BoletosAPagar();
+                        break;
+                    case 3:
+                        BoletosVencidos();
+                        break;
+                    default:
+                        break;
+                }
+        }
+
+
+        public static void BoletosPagos()
+        {
+            Console.WriteLine("========== Boletos pagos ============");
+            var boletos = listaBoletos
+                            .Where(item => item.Confirmacao)
+                            .ToList();
+
+            foreach (var item in boletos)
+            {
+                Console.WriteLine("\n ====");
+                Console.WriteLine($"Codigo de Barra: {item.CodigoBarra}\nValor:{item.Valor}\nData Pagamento: {item.DataPagamento} ==");
+            }
+
+            Console.WriteLine("========== Boletos pagos ============ \n");
+        }
+
+        public static void BoletosAPagar()
+        {
+            Console.WriteLine("========== Boletos à pagar ============");
+            var boletos = listaBoletos
+                            .Where(item => item.Confirmacao == false
+                                    && item.DataVencimento > DateTime.Now)
+                            .ToList();
+
+            foreach (var item in boletos)
+            {
+                Console.WriteLine("\n ====");
+                Console.WriteLine($"Codigo de Barra: {item.CodigoBarra}\nValor:{item.Valor}\nData Pagamento: {item.DataPagamento} ==");
+            }
+
+            Console.WriteLine("========== Boletos à pagar ============ \n");
+        }
+
+        public static void BoletosVencidos()
+        {
+            Console.WriteLine("========== Boletos vencidos ============");
+            var boletos = listaBoletos
+                            .Where(item => item.Confirmacao == false
+                                    && item.DataVencimento < DateTime.Now)
+                            .ToList();
+
+            foreach (var item in boletos)
+            {
+                Console.WriteLine("\n ====");
+                Console.WriteLine($"Codigo de Barra: {item.CodigoBarra}\nValor:{item.Valor}\nData Pagamento: {item.DataPagamento} ==");
+            }
+
+            Console.WriteLine("========== Boletos vencidos ============ \n");
+        }
+    }
+}
+
